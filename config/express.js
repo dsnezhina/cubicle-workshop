1f0fc8df0eb12e230f40812e7d19981d3f5f4573
dsnezhina <66200617+dsnezhina@users.noreply.github.com>
2020-06-07 12:00:04 +0300

    feat: update handlebars templates and add new ones

--- config/express.js
+++ config/express.js
@@ -3,14 +3,14 @@ const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 module.exports = (app) => {
+
     //Setup the view engine
     app.engine('.hbs', handlebars({
         extname: ".hbs"
     }));
     app.set('view engine', '.hbs');
 
-    //Setup the body parser
-    app.use(express.urlencoded())
+    //TODO: Setup the body parser
 
     //Setup the static files
     app.use('/static', express.static('./static'))
