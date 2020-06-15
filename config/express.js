15cce12f784e01b46b71eb0c9a7f10c3e4d4bc36
dsnezhina <66200617+dsnezhina@users.noreply.github.com>
2020-06-15 10:57:01 +0300

    feat: finish signup

--- config/express.js
+++ config/express.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser')
 
 module.exports = (app) => {
+    app.use(cookieParser())
     //Setup the view engine
     app.engine('.hbs', handlebars({
         extname: ".hbs"
